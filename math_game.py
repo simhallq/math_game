9640f38201c72444ea41c000afcdcb5d779d181d
simhallq <hallqvist.simon@gmail.com>
2022-11-13 14:18:40 +0100

    class version

--- math_game.py
+++ math_game.py
@@ -1,35 +1,90 @@
+import inquirer
+from playsound import playsound
 from numpy import random
 import time
+import gtts
 
 
-def generate_question(type):
+class MathGame:
+    def __init__(self):
+        self.sound_setting = inquirer.prompt(
+            [
+                inquirer.List(
+                    "sound_setting",
+                    message="Choose a setting",
+                    choices=["Only sound", "Only text", "Sound and text"],
+                )
+            ]
+        )["sound_setting"]
+        self.mode = inquirer.prompt(
+            [
+                inquirer.List(
+                    "mode",
+                    message="Choose a mode",
+                    choices=["Approximate", "Exact"],
+                )
+            ]
+        )["mode"]
 
-    if type == "approx_multi":
-        return random.randint(1e2, 1e4), random.randint(1e2, 1e4)
+        n_rounds = inquirer.prompt(
+            [
+                inquirer.Text(
+                    "n_rounds",
+                    message="How many rounds do you want to play? Default is 12",
+                    # validate=lambda _, x: x.isdigit() and int(x) > 0,
+                )
+            ]
+        )["n_rounds"]
+        if n_rounds.isdigit():
+            n_rounds = int(n_rounds)
+        else:
+            n_rounds = 12
 
+        self.n_rounds = n_rounds
 
-def check_answer(type, answer, a, b):
-    if type == "approx_multi":
-        return (answer <= a * b * 1.1) or (answer >= a * b * 0.9)
+    def generate_question(self):
+        if self.mode == "Approximate":
+            return random.randint(1e2, 1e4), random.randint(1e2, 1e4)
+
+    def check_answer(self, answer, a, b):
+        if self.mode == "Approximate":
+            return (answer <= a * b * 1.1) or (answer >= a * b * 0.9)
+
+    def start_round(self):
+        a, b = self.generate_question()
+
+        question = f"{a} gånger {b}? "
+        while True:
+            if self.sound_setting == "Only text":
+                answer = input(question)
+            elif self.sound_setting == "Only sound":
+                gtts.gTTS(question, lang="sv").save("question.mp3")
+                playsound("question.mp3")
+                answer = input("Answer: ")
+            elif self.sound_setting == "Sound and text":
+                gtts.gTTS(question, lang="sv").save("question.mp3")
+                playsound("question.mp3")
+                answer = input(question)
+
+            if answer.isdigit():
+                answer = int(answer)
+                if self.check_answer(answer, a, b):
+                    print("Correct!")
+                    break
+                else:
+                    print("Wrong! Try again.")
+            elif answer == "r" and self.sound_setting != "Only text":
+                continue
+            elif answer == "q":
+                break
+            else:
+                print("Invalid answer. Try again.")
 
 
 def main():
-    n_questions = 20
-    n_correct = 0
-
-    for i in range(n_questions):
-        a, b = generate_question("approx_multi")
-        print(f"Question: {a}*{b}")
-        start = time.time()
-        answer = int(input("Answer: "))
-        end = time.time()
-        if check_answer("approx_multi", answer, a, b):
-            n_correct += 1
-            print(f"Correct! Took {end-start:.2f} seconds")
-        else:
-            print(f"Incorrect! Exact answer is {a*b}, you answered {answer}")
-
-    pass
+    game = MathGame()
+    for i in range(game.n_rounds):
+        game.start_round()
 
 
 if __name__ == "__main__":
